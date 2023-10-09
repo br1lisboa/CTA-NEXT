93afe479c2ac9848429155d631b2eaf663fe22a9
Gianluca Fava <101229939+Gianluca1796@users.noreply.github.com>
2023-10-09 16:22:28 -0300

    modificaciones en dependencias

--- src/pages/noticias/[id].tsx
+++ src/pages/noticias/[id].tsx
@@ -1,5 +1,5 @@
 
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from 'firebase/firestore';
 import { db } from "../../firebase/config";
 
 import { MainLayout } from '@/layouts/MainLayout';
